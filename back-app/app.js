2eb02a51fd8a85109966ac358db72769b7503f19
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2025-01-09 14:57:59 +0100

    add: created server

---
+++ back-app/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT
+const host = process.env.HOST
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Benvenuto")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on ${host}:${port}`);
+})
