fd983ae5f5d57d8c43a1193a96934b600d326122
Alberto Monaco <alberto.monaco84@gmail.com>
2025-01-13 16:16:10 +0100

    add homepage

--- front-app/src/pages/Homepage.jsx
+++ front-app/src/pages/Homepage.jsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from 'react'
+
 export default function Homepage() {
+	const [properties, setProperties] = useState([])
+	const url = 'http://localhost:3000/api/properties'
+	function fetchData() {
+		fetch(url)
+			.then((res) => res.json())
+			.then((response) => setProperties(response))
+	}
+	useEffect(() => {
+		fetchData()
+	}, [])
+
+	console.log(properties)
+
 	return (
-		<div>
+		<>
 			<h1>Homepage</h1>
-		</div>
+			{properties?.map((property) => (
+				<div key={property.id}>
+					<h2>{property.name}</h2>
+					<p>{property.address}</p>
+				</div>
+			))}
+		</>
 	)
 }
