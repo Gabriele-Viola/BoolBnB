45226ac7d977ba33b55d3c36c08c3d1de0fb4586
Efestus31 <biagiohd@gmail.com>
2025-01-14 11:30:06 +0100

    fix about and contact pages

--- front-app/src/pages/Contact.jsx
+++ front-app/src/pages/Contact.jsx
@@ -1,13 +1,15 @@
 export default function Contact() {
 	return (
-		<div className="container py-5">
-			<h1 className="mb-4">Contattaci</h1>
-			<div className="row">
-				<div className="col-12">
-					<p>Hai domande o suggerimenti? Siamo qui per aiutarti!</p>
-					<p>Email: info@boolbnb.it | Telefono: +39 02 1234567</p>
+		<main>
+			<div className="container py-5">
+				<h1 className="mb-4">Contattaci</h1>
+				<div className="row">
+					<div className="col-12">
+						<p>Hai domande o suggerimenti? Siamo qui per aiutarti!</p>
+						<p>Email: info@boolbnb.it | Telefono: +39 02 1234567</p>
+					</div>
 				</div>
 			</div>
-		</div>
+		</main>
 	)
 }
