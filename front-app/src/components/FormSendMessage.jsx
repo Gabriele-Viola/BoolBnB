9948bc10b137e3512fa81f3cf5fe5edfa792f350
Alberto Monaco <alberto.monaco84@gmail.com>
2025-01-14 15:56:15 +0100

    fix form control

--- front-app/src/components/FormSendMessage.jsx
+++ front-app/src/components/FormSendMessage.jsx
@@ -1,18 +1,54 @@
-export default function FormSendMessage({ HandleinputToggle, HandleSubMessage, emailUser, setEmailUser, textUser, setTextUser }) {
-    return (
-        <div id="newMessage" className="d-none vw-100 vh-100 d-flex justify-content-center align-items-center position-fixed top-0 start-0 bg-secondary bg-opacity-75 ">
-
-            <div className="mt-5 rounded bg-primary w-50 p-4 shadow position-relative">
-                <button className="text-light position-absolute end-0 top-0 m-4 fs-3 btn" onClick={() => HandleinputToggle('newMessage')}><i className="bi bi-x-circle"></i></button>
-                <h3 className="mb-4 text-light">Send your message</h3>
-                <form className="newMessage" onSubmit={HandleSubMessage}>
-                    <label htmlFor="email" className="form-label text-light">Your email</label>
-                    <input type="email" id='email' name='email' value={emailUser} onChange={(e) => setEmailUser(e.target.value)} className="form-control mb-3" placeholder="Your email address" />
-                    <label htmlFor="message" className="form-label text-light">Your message</label>
-                    <textarea rows="5" className="form-control mb-3" name="message" id="message" value={textUser} onChange={(e) => setTextUser(e.target.value)} placeholder="Your message"></textarea>
-                    <button className="btn btn-light text-primary" type="submit"><i className="bi bi-send-fill"></i> Send</button>
-                </form>
-            </div>
-        </div>
-    )
+export default function FormSendMessage({
+	HandleinputToggle,
+	HandleSubMessage,
+	emailUser,
+	setEmailUser,
+	textUser,
+	setTextUser
+}) {
+	return (
+		<div
+			id="newMessage"
+			className="d-none vw-100 vh-100 d-flex justify-content-center align-items-center position-fixed top-0 start-0 bg-secondary bg-opacity-75 ">
+			<div className="mt-5 rounded bg-primary w-50 p-4 shadow position-relative">
+				<button
+					className="text-light position-absolute end-0 top-0 m-4 fs-3 btn"
+					onClick={() => HandleinputToggle('newMessage')}>
+					<i className="bi bi-x-circle"></i>
+				</button>
+				<h3 className="mb-4 text-light">Send your message</h3>
+				<form className="newMessage" onSubmit={HandleSubMessage}>
+					<label htmlFor="email" className="form-label text-light">
+						Your email
+					</label>
+					<input
+						type="email"
+						id="email"
+						name="email"
+						required
+						value={emailUser}
+						onChange={(e) => setEmailUser(e.target.value)}
+						className="form-control mb-3"
+						placeholder="Your email address"
+					/>
+					<label htmlFor="message" className="form-label text-light">
+						Your message
+					</label>
+					<textarea
+						rows="5"
+						className="form-control mb-3"
+						name="message"
+						required
+						minLength={10}
+						id="message"
+						value={textUser}
+						onChange={(e) => setTextUser(e.target.value)}
+						placeholder="Your message"></textarea>
+					<button className="btn btn-light text-primary" type="submit">
+						<i className="bi bi-send-fill"></i> Send
+					</button>
+				</form>
+			</div>
+		</div>
+	)
 }
--- front-app/src/pages/DetailsPage.jsx
+++ front-app/src/pages/DetailsPage.jsx
@@ -1,43 +1,40 @@
-import { useParams } from "react-router-dom"
-import { useState, useEffect } from "react"
-import DetailsCard from "../components/DetailsCard"
-import ReviewsCard from "../components/ReviewsCard"
-import FormAddReview from "../components/FormAddReview"
-import FormSendMessage from "../components/FormSendMessage"
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import DetailsCard from '../components/DetailsCard'
+import ReviewsCard from '../components/ReviewsCard'
+import FormAddReview from '../components/FormAddReview'
+import FormSendMessage from '../components/FormSendMessage'
 
 export default function DetailsPage() {
-
 	const { id } = useParams()
 	const [property, setProperty] = useState({})
 	const [services, setServices] = useState([])
 	const [reviews, setReviews] = useState([])
-	const [nameUser, setNameUser] = useState("")
-	const [nights, setNights] = useState("")
-	const [review, setReview] = useState("")
-	const [emailUser, setEmailUser] = useState("")
-	const [textUser, setTextUser] = useState("")
-
+	const [nameUser, setNameUser] = useState('')
+	const [nights, setNights] = useState('')
+	const [review, setReview] = useState('')
+	const [emailUser, setEmailUser] = useState('')
+	const [textUser, setTextUser] = useState('')
+	const [feedback, setFeedback] = useState('')
 
 	const urlShow = `http://localhost:3000/api/properties/${id}`
 	const urlreviews = `http://localhost:3000/api/${id}/reviews`
 
 	useEffect(() => {
-
 		fetch(urlShow)
-			.then((res => res.json()))
-			.then(data => {
+			.then((res) => res.json())
+			.then((data) => {
 				setProperty(data.property)
 				setServices(data.property.services)
-			}).catch(err => console.error(err))
+			})
+			.catch((err) => console.error(err))
 
 		fetch(urlreviews)
-			.then(res => res.json())
-			.then(data => {
+			.then((res) => res.json())
+			.then((data) => {
 				setReviews(data.reviews)
 			})
-
-	}, [id])
-
+	}, [id, reviews])
 
 	function HandleSubReview(e) {
 		e.preventDefault()
@@ -52,28 +49,33 @@ export default function DetailsPage() {
 		}
 
 		fetch(urlPostReview, {
-			method: "POST",
+			method: 'POST',
 			body: JSON.stringify(formReview),
 			headers: {
-				"Content-Type": "application/json"
+				'Content-Type': 'application/json'
 			}
-
-		}).then(res => res.json())
-			.then(data => {
+		})
+			.then((res) => res.json())
+			.then((data) => {
 				console.log(data)
-			}).catch(err => {
-				console.error(err)
+				setFeedback('Your review has been sent!')
+				setTimeout(() => {
+					setFeedback('')
+				}, 3000)
 			})
-		setNameUser("")
-		setNights("")
-		setReview("")
+			.catch((err) => {
+				console.error(err)
+				setFeedback('Error sending your review')
+			})
+		setNameUser('')
+		setNights('')
+		setReview('')
 	}
-
 
 	function HandleSubMessage(e) {
 		e.preventDefault()
 
-		const urlPostMessage = "http://localhost:3000/api/message/send"
+		const urlPostMessage = 'http://localhost:3000/api/message/send'
 
 		const formMessage = {
 			id_property: id,
@@ -82,20 +84,21 @@ export default function DetailsPage() {
 		}
 
 		fetch(urlPostMessage, {
-			method: "POST",
+			method: 'POST',
 			body: JSON.stringify(formMessage),
 			headers: {
-				"Content-Type": "application/json"
+				'Content-Type': 'application/json'
 			}
-
-		}).then(res => res.json())
-			.then(data => {
+		})
+			.then((res) => res.json())
+			.then((data) => {
 				console.log(data)
-			}).catch(err => {
+			})
+			.catch((err) => {
 				console.error(err)
 			})
-		setEmailUser("")
-		setTextUser("")
+		setEmailUser('')
+		setTextUser('')
 	}
 
 	function HandleinputToggle(item) {
@@ -110,23 +113,40 @@ export default function DetailsPage() {
 						<h1>Property details</h1>
 					</div>
 					<div className="col-6 text-end">
-						<button type="button" className="btn btn-primary" onClick={() => HandleinputToggle('newMessage')}>Send Message</button>
+						<button type="button" className="btn btn-primary" onClick={() => HandleinputToggle('newMessage')}>
+							Send Message
+						</button>
 					</div>
 				</div>
 			</div>
 			<div className="container">
-
 				<DetailsCard property={property} services={services} />
 				<div className="reviews mt-5">
 					<h3>Reviews</h3>
 					<div className="row g-3">
 						<ReviewsCard reviews={reviews} />
+						<div>{feedback && <div className="alert alert-success mt-3">{feedback}</div>}</div>
 					</div>
 
-					<FormAddReview HandleSubReview={HandleSubReview} nameUser={nameUser} setNameUser={setNameUser} nights={nights} setNights={setNights} review={review} setReview={setReview} />
+					<FormAddReview
+						HandleSubReview={HandleSubReview}
+						nameUser={nameUser}
+						setNameUser={setNameUser}
+						nights={nights}
+						setNights={setNights}
+						review={review}
+						setReview={setReview}
+					/>
 				</div>
 			</div>
-			<FormSendMessage HandleinputToggle={HandleinputToggle} HandleSubMessage={HandleSubMessage} emailUser={emailUser} setEmailUser={setEmailUser} textUser={textUser} setTextUser={setTextUser} />
+			<FormSendMessage
+				HandleinputToggle={HandleinputToggle}
+				HandleSubMessage={HandleSubMessage}
+				emailUser={emailUser}
+				setEmailUser={setEmailUser}
+				textUser={textUser}
+				setTextUser={setTextUser}
+			/>
 		</div>
 	)
 }
