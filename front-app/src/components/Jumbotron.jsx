57dd94b404f6ce733c1caa1a06fff9503ee058d3
Alberto Monaco <alberto.monaco84@gmail.com>
2025-01-16 14:02:43 +0100

    fix jumbotron

--- front-app/src/components/Jumbotron.jsx
+++ front-app/src/components/Jumbotron.jsx
@@ -1,7 +1,10 @@
-export default function Banner({ title }) {
+export default function Jumbotron({ title, subtitle, className = '' }) {
 	return (
-		<div className="banner bg-primary text-white p-4 mb-5 rounded">
-			<h1>{title}</h1>
+		<div className={`banner bg-primary text-white p-5 mb-5 rounded shadow-lg ${className}`}>
+			<div className="container">
+				<h3 className="">{title}</h3>
+				{subtitle && <p className="lead mt-3">{subtitle}</p>}
+			</div>
 		</div>
 	)
 }
