015557849fd26adab70eb32b8daecca14f176a44
Alberto Monaco <alberto.monaco84@gmail.com>
2025-01-20 16:00:00 +0100

    add review form invisible after review

--- front-app/src/components/FormAddReview.jsx
+++ front-app/src/components/FormAddReview.jsx
@@ -5,8 +5,7 @@ export default function FormAddReview({
 	nights,
 	setNights,
 	review,
-	setReview,
-	feedback
+	setReview
 }) {
 	// Aggiungiamo una funzione per verificare la validità dei campi
 	const isFormValid = () => {
@@ -15,57 +14,54 @@ export default function FormAddReview({
 
 	return (
 		<>
-			{' '}
-			{!feedback && (
-				<div className="mt-3 p-3 border border-primary-subtle rounded">
-					<h3 className="mb-2 text-center">Lascia una recensione</h3>
-					<form className="newReview" onSubmit={HandleSubReview}>
-						<label htmlFor="name" className="form-label">
-							Nome
-						</label>
-						<input
-							type="text"
-							id="name"
-							name="name"
-							required
-							minLength={3}
-							value={nameUser}
-							onChange={(e) => setNameUser(e.target.value)}
-							className="form-control mb-3"
-							placeholder="Inserisci il tuo nome (minimo 3 caratteri)"
-						/>
-						<label htmlFor="nights" className="form-label">
-							Notti
-						</label>
-						<input
-							type="number"
-							id="nights"
-							name="nights"
-							required
-							min={1}
-							value={nights}
-							onChange={(e) => setNights(e.target.value)}
-							className="form-control mb-3"
-							placeholder="Per quante notti hai soggiornato (minimo 1)"
-						/>
-						<label htmlFor="review" className="form-label">
-							La tua recensione:
-						</label>
-						<textarea
-							className="form-control mb-3"
-							name="review"
-							id="review"
-							required
-							minLength={10}
-							value={review}
-							onChange={(e) => setReview(e.target.value)}
-							placeholder="Inserisci la tua recensione (minimo 10 caratteri)"></textarea>
-						<button className="btn btn-primary" type="submit" disabled={!isFormValid()}>
-							<i className="bi bi-send-fill"></i> Invia!
-						</button>
-					</form>
-				</div>
-			)}
+			<div className="mt-3 p-3 border border-primary-subtle rounded">
+				<h3 className="mb-2 text-center">Lascia una recensione</h3>
+				<form className="newReview" onSubmit={HandleSubReview}>
+					<label htmlFor="name" className="form-label">
+						Nome
+					</label>
+					<input
+						type="text"
+						id="name"
+						name="name"
+						required
+						minLength={3}
+						value={nameUser}
+						onChange={(e) => setNameUser(e.target.value)}
+						className="form-control mb-3"
+						placeholder="Inserisci il tuo nome (minimo 3 caratteri)"
+					/>
+					<label htmlFor="nights" className="form-label">
+						Notti
+					</label>
+					<input
+						type="number"
+						id="nights"
+						name="nights"
+						required
+						min={1}
+						value={nights}
+						onChange={(e) => setNights(e.target.value)}
+						className="form-control mb-3"
+						placeholder="Per quante notti hai soggiornato (minimo 1)"
+					/>
+					<label htmlFor="review" className="form-label">
+						La tua recensione:
+					</label>
+					<textarea
+						className="form-control mb-3"
+						name="review"
+						id="review"
+						required
+						minLength={10}
+						value={review}
+						onChange={(e) => setReview(e.target.value)}
+						placeholder="Inserisci la tua recensione (minimo 10 caratteri)"></textarea>
+					<button className="btn btn-primary" type="submit" disabled={!isFormValid()}>
+						<i className="bi bi-send-fill"></i> Invia!
+					</button>
+				</form>
+			</div>
 		</>
 	)
 }
