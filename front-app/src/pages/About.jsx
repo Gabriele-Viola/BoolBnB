e00631c3c7db8d8de56cd53e2e7be584442aa749
Alberto Monaco <alberto.monaco84@gmail.com>
2025-01-20 15:16:51 +0100

    add jumbron on contact and about

--- front-app/src/pages/About.jsx
+++ front-app/src/pages/About.jsx
@@ -1,16 +1,20 @@
+import Jumbotron from '../components/Jumbotron'
 export default function About() {
-
 	return (
-
-		<div className="container py-5">
-			<h1 className="mb-4">Chi Siamo</h1>
-			<div className="row">
-				<div className="col-12">
-					<p>BoolB&B è la tua piattaforma di fiducia per trovare e prenotare alloggi unici in tutta Italia.</p>
-					<p>La nostra missione è connettere viaggiatori e host per creare esperienze indimenticabili.</p>
+		<>
+			<div className="container">
+				<Jumbotron title="Chi siamo" className="mb-5" />
+				<div className="row">
+					<div className="ps-4 col-12">
+						<p className=" fw-bold">
+							BoolB&B è la tua piattaforma di fiducia per trovare e prenotare alloggi unici in tutta Italia.
+						</p>
+						<p className=" fw-bold">
+							La nostra missione è connettere viaggiatori e host per creare esperienze indimenticabili.
+						</p>
+					</div>
 				</div>
 			</div>
-		</div>
-
+		</>
 	)
 }
